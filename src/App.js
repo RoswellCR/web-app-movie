fa536a872091fd9efcca1d1b2998ef7dbb969dbc
RoswellCR <rosbelcr@gmail.com>
2022-08-08 15:36:08 -0400

    adding files to remote repository

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {UserProvider} from './contexts/UserContext';
+import MovieList from './components/MovieList';
+import Navbar from './components/Navbar';
+import { MoviesProvider } from './contexts/MoviesContext';
+
+
+function App() {
+ 
+  
+  return (
+    
+      <div>
+        <UserProvider>
+          <MoviesProvider>
+            <Navbar />
+            <MovieList />
+          </MoviesProvider>
+        </UserProvider>
+      
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/hooks/useUser.js
@@ -0,0 +1,5 @@
+//import { useContext } from "react";     
+//import UserContext from "../context/UserContext";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+//export default  () => useContext(UserContext);
